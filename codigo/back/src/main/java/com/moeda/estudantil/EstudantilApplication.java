5945de8bcf7e562e84f8a704f534a03e8f3eae97
matheustitton <matheustitton@gmail.com>
2025-10-15 23:14:30 -0300

    Iniciando Back

---
+++ codigo/back/src/main/java/com/moeda/estudantil/EstudantilApplication.java
@@ -0,0 +1,13 @@
+package com.moeda.estudantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudantilApplication.class, args);
+	}
+
+}
---
+++ codigo/back/src/test/java/com/moeda/estudantil/EstudantilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moeda.estudantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
