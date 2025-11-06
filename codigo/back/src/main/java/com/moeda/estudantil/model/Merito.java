f2bf0f9b7fcf4e66728f960219cf8f47f895ccc0
matheustitton <matheustitton@gmail.com>
2025-11-05 22:00:24 -0300

    add merito

---
+++ codigo/back/src/main/java/com/moeda/estudantil/model/Merito.java
@@ -0,0 +1,43 @@
+package com.moeda.estudantil.model;
+
+import java.time.OffsetDateTime;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "merito")
+public class Merito {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "aluno_id", nullable = false)
+    private Aluno aluno;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "professor_id", nullable = false)
+    private Professor professor;
+
+    @Column(nullable = false, length = 255)
+    private String motivo;
+
+    @Column(nullable = false)
+    private int valor;
+
+    @Column(nullable = false)
+    private OffsetDateTime data = OffsetDateTime.now();
+
+    public Merito() {}
+    
+}
