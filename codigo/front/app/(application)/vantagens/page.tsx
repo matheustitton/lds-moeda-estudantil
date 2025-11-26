dde94342d8e1680d70aa1f67f825c8804949afb9
matheustitton <matheustitton@gmail.com>
2025-11-25 23:49:50 -0300

    Inicio

---
+++ codigo/front/app/(application)/vantagens/page.tsx
@@ -0,0 +1,11 @@
+import ListaVantagens from "../../vantagens/_components/ListaVantagens";
+
+export default function VantagensPage() {
+    return (
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center sm:items-start w-full">
+                <ListaVantagens />
+            </main>
+        </div>
+    );
+}
